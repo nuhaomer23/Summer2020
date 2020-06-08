1663e2cc164743b455f8c840beaa6b8f7e391879
nuhaomer23 <66275886+nuhaomer23@users.noreply.github.com>
2020-06-08 16:07:21 -0500

    Update Calculation.java

--- src/com/company/Calculation.java
+++ src/com/company/Calculation.java
@@ -1,7 +1,22 @@
 package com.company;
 
 public class Calculation {
-    public static void main (String []){
+    public static void main (String [] args) {
+        double dish1= 38;
+        double dish2 = 40;
+        double dish3 = 30;
+        double subtotal = dish1 + dish2 + dish3;
+        final double Tax =0.08;
+        final double Tip = 0.15;
+        double totalprice = subtotal + subtotal* Tax + subtotal* Tip;
+        double totaltax = subtotal*Tax;
+        double totaltip = subtotal*Tip;
+
+
+        System.out.println("Subtotal:"+subtotal);
+        System.out.println("Tax:"+totaltax);
+        System.out.println("Tip:"+totaltip);
+        System.out.println("Total:"+totalprice);
 
     }
 }

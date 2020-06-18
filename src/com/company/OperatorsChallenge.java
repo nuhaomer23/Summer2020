b832508c7b5c4f24aeca58de4d8adb5edce61b29
nuhaomer23 <omern23@greenhil.org>
2020-06-17 20:33:51 -0400

    Challenge Operators v2

--- src/com/company/OperatorsChallenge.java
+++ src/com/company/OperatorsChallenge.java
@@ -1,5 +1,17 @@
 package com.company;
 import java.util.Scanner;
+/*Nuha Omer
+First set up the static double solve to set up the
+calculations for the tip and the tax and the total cost
+calculations with int int for tax and tip and double for total cost but then round
+the set up a public static void main to input the scanner system
+so that the user can input the money
+print What is the meal cost? with nextDouble
+print How much do you want to tip? with nextInt
+print What is the tax? with nextInt
+then in the main call on solve with the calculations to find the total meal cost rounded integers.
+
+ */
 public class OperatorsChallenge {
     Scanner input = new Scanner(System.in);
     static double solve(double mealcst, int tip, int tax) {

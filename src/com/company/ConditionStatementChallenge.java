4ff9332873ccc6ffe0c6732b649c1921eb60bd49
nuhaomer23 <omern23@greenhil.org>
2020-06-19 15:41:55 -0400

    4 ConditionStatementChallenge

--- src/com/company/ConditionStatementChallenge.java
+++ src/com/company/ConditionStatementChallenge.java
@@ -22,32 +22,32 @@ public class ConditionStatementChallenge {
 
     public static void main (String [] args) {
         Scanner Weird = new Scanner(System.in);
-        Random rando = new Random();
+        Random rando = new Random(); // creating the random scanner
 
         String answer;
        do {
            System.out.println("Enter the integer number to see if it's weird! ");
-           int number = rando.nextInt(137)+1;
+           int number = rando.nextInt(137)+1; // the limit between the bounds of the numbers
            System.out.println("This is the inputted number: " + number);
-           ifstatements(number);
+           ifstatements(number); // this is where the random number is input into the program
            System.out.println("Do you want to quit? ");
            answer = Weird.next();
 
        }
-       while (answer.contains("n"));
+       while (answer.contains("y")); // while statement went into after you say you want to quit
         String input = "Thank you for your number!";
         System.out.println(input);
     }
     public static void ifstatements (int number){
-        if (number % 2 == 1) {
-            System.out.println("Weird");
+        if (number % 2 == 1) {// if method for the statements
+            System.out.println("Weird"); // when it is an odd number
         } else if (number % 2 == 0) {
             if (number >= 2 && number <= 5) {
-                System.out.println("Not Weird");
+                System.out.println("Not Weird"); // when its even and between 2 and 5
             } else if (number >= 6 && number <= 20) {
-                System.out.println("Weird");
+                System.out.println("Weird"); // when the number is between 6 and 20
             } else if (number > 20 && number <=137) {
-                System.out.println("Not Weird");
+                System.out.println("Not Weird"); // when number is even and between 20 and 135
             }
         }
     }
